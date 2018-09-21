444f5b97f5f865fed2581d9146e5e94e12afec75
Alexander Varwijk <alexander@goalgorilla.com>
2018-09-21 16:29:00 +0200

    Add support for compiling SCSS to CSS
    This replicates the work that is currently being done for SCSS in
    socialbase/socialblue using gulp. The output of the css files as
    tested is identical to that of the current gulp workflow.
    
    It also adds some simple clean commands.

--- cli.js
+++ cli.js
@@ -1,6 +1,34 @@
 #! /usr/bin/env node
+"use strict";
 
 const toolInfo = require('./package.json');
+const { assertUsage } = require('./lib/usage');
+const DrupalTheme = require('./lib/DrupalTheme');
+const resolveCommand = require('./lib/commands');
 
 // We always print version info.
-console.log(`${toolInfo.name} version ${toolInfo.version}`);
+console.log(`${toolInfo.name} v${toolInfo.version}`);
+
+// Strip the node executable and cli filename from the arguments.
+const args = process.argv.slice(2);
+
+// Require at least a single argument to indicate the user action.
+assertUsage(args.length > 0);
+
+// The first argument must be a command (not prefixed with "-" or "--").
+const command = args[0];
+assertUsage(command.substr(0, 1) !== "-");
+
+// Resolve the provided command to an action that we can execute.
+// TODO: Parse args here instead of in resolveCommand.
+const action = resolveCommand(command, args.slice(1));
+
+// It could be that an action was requested that could be executed without
+// loading a theme. In that case we're all done.
+if (!action) {
+  process.exit(0);
+}
+
+const Theme = DrupalTheme.loadFromDirectory(process.cwd());
+
+action(Theme);
