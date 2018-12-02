6d2dce040a32fdcb9d4ed451463e1c23a883fef5
Alexander Varwijk <alexander@goalgorilla.com>
2018-12-02 22:06:49 +0100

    test: add test data for testing theme loading
    This commit adds fixture data that will allow the theme loading to be
    tested as well as test inheritance and library configuration at a later
    point.

--- packages/sweet-pea-core/src/DrupalTheme.test.js
+++ packages/sweet-pea-core/src/DrupalTheme.test.js
@@ -11,4 +11,13 @@ describe('Drupal Theme', () => {
     const Theme = new DrupalTheme(__dirname);
     expect(() => Theme.getOutputPath('nonexistant')).toThrow();
   });
+
+  test('can load a theme from a valid directory', () => {
+    const Theme = DrupalTheme.loadFromDirectory(path.join(__dirname, "../fixtures/themeA"));
+    expect(Theme.name).toBe('themeA');
+  });
+
+  test('throws an error when it can\'t find a theme', () => {
+    expect(() => DrupalTheme.loadFromDirectory(path.join(__dirname, "../fixtures/canteverexist"))).toThrow();
+  })
 });
